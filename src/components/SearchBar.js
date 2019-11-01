a41cd15264685842ada8099573623dcbc8e4746c
Agne Nevulyte <agne.nevulyte@finsaeurope.com>
2019-11-01 11:37:28 +0000

    added bootsrap

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,8 +2,19 @@ import React from "react";
 
 export default function SearchBar({ handleChange, input }) {
   return (
-    <div>
-      <input value={input} onChange={handleChange} />
+    <div className="container">
+      <div className="row d-flex justify-content-center">
+        <div className="input-group mb-3 col-sm-6">
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"
+            value={input}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
     </div>
   );
 }
